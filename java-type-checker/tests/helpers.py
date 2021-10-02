a6cbec7ade8e41d48f11af647cbba9d1943acac0
Paul Cantrell <cantrell@pobox.com>
2021-10-01 22:02:54 -0500

    More helpful failure reports for assertCompileError

--- java-type-checker/tests/helpers.py
+++ java-type-checker/tests/helpers.py
@@ -1,11 +1,19 @@
 import unittest
-import re
+import sys
+from java_type_checker import JavaTypeError
 
 
 class TypeTest(unittest.TestCase):
     def assertCompileError(self, error, error_message, expr):
-        with self.assertRaisesRegex(error, "^" + re.escape(error_message) + "$"):
-            expr.check_types()
+        try:
+            with self.assertRaises(error) as exception_context:
+                expr.check_types()
+            self.assertEqual(error_message, str(exception_context.exception))
+        except Exception as actual_error:
+            sys.stderr.write(
+                "Got " + type(actual_error).__name__ + ", but expected " + error.__name__
+                + "; did you raise the wrong exception type?")
+            raise
 
     def assertNoCompileErrors(self, expr):
         expr.check_types()

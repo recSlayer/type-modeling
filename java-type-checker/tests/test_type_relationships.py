655b8095aceba42aebda2055531f421e2235d150
Paul Cantrell <cantrell@pobox.com>
2021-10-02 22:39:10 -0500

    More rigorous testing for indirect subtypes

--- java-type-checker/tests/test_type_relationships.py
+++ java-type-checker/tests/test_type_relationships.py
@@ -12,13 +12,24 @@ class TestTypeRelationships(unittest.TestCase):
 
     def test_01_subtype_includes_direct_supertypes(self):
         self.assert_subtype(Graphics.graphics_group, Graphics.graphics_object)
+        self.assert_subtype(Graphics.rectangle, Graphics.fillable)
         self.assert_not_subtype(Graphics.graphics_object, Graphics.graphics_group)
+        self.assert_not_subtype(Graphics.fillable, Graphics.rectangle)
 
     def test_02_subtype_includes_indirect_supertypes(self):
         self.assert_subtype(Graphics.color, JavaBuiltInTypes.OBJECT)
         self.assert_not_subtype(JavaBuiltInTypes.OBJECT, Graphics.color)
 
-    def test_03_subtype_does_not_include_unrelated_types(self):
+    def test_03_subtype_handles_arbitrary_levels_of_indirection(self):
+        deep_subtype = Graphics.rectangle
+        for i in range(100):
+            unrelated_type = JavaObjectType("UnrelatedType{0}".format(i))
+            deep_subtype = JavaObjectType(
+                "DeepType{0}".format(i),
+                direct_supertypes=[unrelated_type, deep_subtype])
+        self.assert_subtype(deep_subtype, Graphics.strokable)
+
+    def test_04_subtype_does_not_include_unrelated_types(self):
         self.assert_not_subtype(Graphics.color, Graphics.point)
         self.assert_not_subtype(Graphics.point, Graphics.color)
 
